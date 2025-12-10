de8111de45b4d861b48849dc3ece2f50f08e8c7c
Brendan Hom <hombm@dukes.jmu.edu>
2025-12-10 17:40:12 -0500

    update settings

--- travel_activity/travel_activity/settings.py
+++ travel_activity/travel_activity/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/home/Brendow/CS5614-Travel-Activity/static"
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT  = "/home/Brendow/CS5614-Travel-Activity/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
